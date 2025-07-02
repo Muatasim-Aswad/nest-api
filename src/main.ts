43587d46008145cfc5947076605c30e9daba0728
Muatasim-Aswad <malasoad2021@gmail.com>
2025-07-02 13:29:24 +0200

    expermenting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS for all routes
+  app.setGlobalPrefix('api'); // Set a global prefix for all routes
+  app.enableVersioning();
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error('Error during bootstrap:', error);
+  process.exit(1); // Exit the process with a failure code
+});
